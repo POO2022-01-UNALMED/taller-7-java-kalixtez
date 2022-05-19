9ac46c95a08e9b52f92ef9d67dd981b78b3da78a
kalixtez <64610615+kalixtez@users.noreply.github.com>
2022-05-18 21:07:58 -0500

    sdfs

--- src/main/java/comunicacion/Periodico.java
+++ src/main/java/comunicacion/Periodico.java
@@ -45,7 +45,7 @@ public class Periodico extends Escrito{
 	
 	public String toString()
 	{
-		return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.fecha + "\n" + this.interpretacion;
+		return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.fecha + "\n" + this.primicia;
 	}
 
 
--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -29,7 +29,7 @@ public class Tesis extends Escrito
 	
 	public String toString()
 	{
-		return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.idea + "\n" + this.argumentos + "\n" + this.conclusion + "\n" + this.referencia;
+		return super.getOrigen() + "\n" + super.getTitulo() + "\n" + super.getAutor() + "\n" + super.getPaginas() + "\n" + this.idea + "\n" + this.argumentos.length + "\n" + this.conclusion + "\n" + this.referencia;
 	}
 
 	public int palabrasTotales(int palabrasPagina) {
